0e2931e08ed6893599f4974edfa1b742aa333417
juansadeveloper <juansapereira@hotmail.com>
2024-06-13 09:57:04 +0200

    Update faq page

--- app/faq/page.jsx
+++ app/faq/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Faq from '../components/Faq'
+// import Faq from '../components/Faq'
 
 function page() {
   return (
     <main className='h-[75vh] flex flex-col justify-center items-center'>
         <h1 className='font-bold text-xl'>PREGUNTAS FRECUENTES</h1>
-        <Faq />
+        {/* <Faq /> */}
     </main>
   )
 }
