c528dc7012447bc4bc90665a5ea7ccb46b2e4e72
juansadeveloper <juansapereira@hotmail.com>
2024-06-20 10:21:04 +0200

    uopdated

--- app/feedback/page.jsx
+++ app/feedback/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <main className='h-[75vh] text-center p-5 flex flex-col justify-center items-center'>
+    <main className='md:h-[75vh] text-center p-5 flex flex-col justify-center items-center'>
         <h3 className='font-bold text-xl'>Gracias por ayudarnos a mejorar.</h3>
         <p className='text-lg'>Para poder hacerlo solo tienes que llenar el siguiente formulario.</p>
         <form className='flex flex-col gap-3 p-5 mt-10 max-w-[400px] w-full bg-container rounded-md border'>
